d67d84b67aa3784d3db260bab62d17085647d628
Kolya Efimov <999vaz00@mail.ru>
2025-11-20 20:26:01 +0300

    connected-homework-8-js

---
+++ product-cards.js
@@ -0,0 +1,62 @@
+export const productCards = [
+{
+  img: '/assets/muss.png',
+  category: 'для нормальной кожи',
+  name: 'Увлажняющий мусс',
+  description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
+  compound: 'Состав:',
+  compoundItem1: "активные натуральные компоненты",
+  compoundItem2: "витамин C, A, PP, B и E",
+  compoundItem3: "солнцезащитные компоненты",
+  priceLabel: "Цена",
+  price: '2750 &#8381'
+},
+{
+  img: '/assets/mask.png',
+  category: 'для нормальной кожи',
+  name: 'Увлажняющая маска',
+  description: 'Способствует удерживанию влаги в верхних слоях кожи.',
+  compound: 'Состав:',
+  compoundItem1: "воски",
+  compoundItem2: "минералы",
+  compoundItem3: "масла",
+  priceLabel: "Цена",
+  price: '3500 &#8381'
+},
+{
+  img: '/assets/gel.png',
+  category: 'для нормальной кожи',
+  name: 'Гель для умывания',
+  description: 'Интенсивно очищает, не повреждает защитный барьер кожи.',
+  compound: 'Состав:',
+  compoundItem1: "воски",
+  compoundItem2: "минералы",
+  compoundItem3: "масла",
+  priceLabel: "Цена",
+  price: '1650 &#8381'
+},
+{
+  img: '/assets/gift-set-2.png',
+  category: 'для нормальной кожи',
+  name: 'Подарочный набор №1',
+  description: 'Набор, состоящий из увлажняющего крема и маски.',
+  compound: 'Состав:',
+  compoundItem1: "воски",
+  compoundItem2: "минералы",
+  compoundItem3: "масла",
+  priceLabel: "Цена",
+  price: '4750 &#8381'
+},
+{
+  img: '/assets/gift-set-5.png',
+  category: 'для нормальной кожи',
+  name: 'Подарочный набор №5',
+  description: 'Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.',
+  compound: 'Состав:',
+  compoundItem1: "воски",
+  compoundItem2: "минералы",
+  compoundItem3: "масла",
+  priceLabel: "Цена",
+  price: '7520 &#8381'
+}
+]
