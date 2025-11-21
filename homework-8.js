761b7640b8a29706b01480271b8b60ca27270cde
Kolya Efimov <999vaz00@mail.ru>
2025-11-21 20:05:40 +0300

    fix-error

--- homework-8.js
+++ homework-8.js
@@ -5,29 +5,34 @@ import { productCards } from "./product-cards.js"
 const productTemplate = document.getElementById('product-card-template');
 const productList = document.getElementById('product-list');
 
+const displaySelectedProductCards = (productCards) => {
 productCards.forEach(productCard => {
   const productCardClone = productTemplate.content.cloneNode(true);
-  productCardClone.querySelector('.product-image').src = productCard.img
+  productCardClone.querySelector('.product-image').src = `/assets/${productCard.img}`
   productCardClone.querySelector('.product-category').textContent = productCard.category
   productCardClone.querySelector('.product-name').textContent = productCard.name
   productCardClone.querySelector('.product-description').textContent = productCard.description
-  productCardClone.querySelector('.product-compound').textContent = productCard.compound
-  productCardClone.querySelector('.product-compound-item-1').textContent = productCard.compoundItem1
-  productCardClone.querySelector('.product-compound-item-2').textContent = productCard.compoundItem2
-  productCardClone.querySelector('.product-compound-item-3').textContent = productCard.compoundItem3
-  productCardClone.querySelector('.product-price-label').textContent = productCard.priceLabel
-  productCardClone.querySelector('.product-price').innerHTML = productCard.price
+  productCardClone.querySelector('.product-compound').textContent  = 'Состав:';
+  const productCompoundList = productCardClone.querySelector('.product-compound-list')
+  productCard.compound.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    productCompoundList.appendChild(li);
+  });
+  productCardClone.querySelector('.product-price-label').textContent = 'Цена'
+  productCardClone.querySelector('.product-price').innerHTML = `${productCard.price} &#8381`
   productList.appendChild(productCardClone)
   console.log(productList)
-})
+}) 
+}
 
 // 4. Используя метод .reduce(), получить строку, которая состоит из названий продуктовых карточек, разделенных точкой с запятой
 
-const namesProductCards = productCards.reduce((sum, productCard) => [...sum, productCard.name], [])
+const productCardsNameList = productCards.reduce((sum, productCard) => [...sum, productCard.name], [])
 
-const namesProductCardsStr = namesProductCards.toString()
+const productCardNames = productCardsNameList.join(';')
 
-console.log(namesProductCardsStr) 
+console.log(productCardNames) 
 
 // 5. Используя метод .reduce(), получить массив объектов, где ключем является название продукта, а значением - его описание
 
@@ -38,32 +43,11 @@ console.log(array)
 // 6*. Реализовать функцию, которая при старте нашей страницы выводит сообщение с текстом, мол "Сколько карточек отобразить? От 1 до 5" и в зависимости от результата - будет выводить это количество. Должна быть защита от введенных других значений (имеется ввиду проверка if).
 
 const askCardsCount = () => {
-  const input = prompt('Сколько карточек отобразить? От 1 до 5');
-  const count = Number(input);
-  const displaySelectedProductCards = productCards.slice(0, count);
-  
-    if (count >= 1 && count <= 5) {
-  const productTemplate = document.getElementById('product-card-template');
-  const productList = document.getElementById('product-list');
+const input = prompt('Сколько карточек отобразить? От 1 до 5');
+const count = Number(input);
+  if (count >= 1 && count <= 5) {
+const selectedProductCards = productCards.slice(0, count);
+displaySelectedProductCards(selectedProductCards)
+  }}
 
-  displaySelectedProductCards.forEach(productCard => {
-  const productCardClone = productTemplate.content.cloneNode(true);
-  productCardClone.querySelector('.product-image').src = productCard.img
-  productCardClone.querySelector('.product-category').textContent = productCard.category
-  productCardClone.querySelector('.product-name').textContent = productCard.name
-  productCardClone.querySelector('.product-description').textContent = productCard.description
-  productCardClone.querySelector('.product-compound').textContent = productCard.compound
-  productCardClone.querySelector('.product-compound-item-1').textContent = productCard.compoundItem1
-  productCardClone.querySelector('.product-compound-item-2').textContent = productCard.compoundItem2
-  productCardClone.querySelector('.product-compound-item-3').textContent = productCard.compoundItem3
-  productCardClone.querySelector('.product-price-label').textContent = productCard.priceLabel
-  productCardClone.querySelector('.product-price').innerHTML = productCard.price
-  productList.appendChild(productCardClone)
-    }); 
-  }
-  return displaySelectedProductCards
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  askCardsCount();
-})
+askCardsCount() 
--- product-cards.js
+++ product-cards.js
@@ -1,62 +1,42 @@
 export const productCards = [
 {
-  img: '/assets/muss.png',
+  img: 'muss.png',
   category: 'для нормальной кожи',
   name: 'Увлажняющий мусс',
   description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
-  compound: 'Состав:',
-  compoundItem1: "активные натуральные компоненты",
-  compoundItem2: "витамин C, A, PP, B и E",
-  compoundItem3: "солнцезащитные компоненты",
-  priceLabel: "Цена",
-  price: '2750 &#8381'
+  compound: ["активные натуральные компоненты", "витамины C, A, PP, B и E", "солнцезащитные компоненты"],
+  price: 2750
 },
 {
-  img: '/assets/mask.png',
+  img: 'mask.png',
   category: 'для нормальной кожи',
   name: 'Увлажняющая маска',
   description: 'Способствует удерживанию влаги в верхних слоях кожи.',
-  compound: 'Состав:',
-  compoundItem1: "воски",
-  compoundItem2: "минералы",
-  compoundItem3: "масла",
-  priceLabel: "Цена",
-  price: '3500 &#8381'
+  compound: ["воски", "минералы", "масла"],
+  price: 3500
 },
 {
-  img: '/assets/gel.png',
+  img: 'gel.png',
   category: 'для нормальной кожи',
   name: 'Гель для умывания',
   description: 'Интенсивно очищает, не повреждает защитный барьер кожи.',
-  compound: 'Состав:',
-  compoundItem1: "воски",
-  compoundItem2: "минералы",
-  compoundItem3: "масла",
-  priceLabel: "Цена",
-  price: '1650 &#8381'
+  compound: ["минералы", "витамины C, A, PP, B и E", "солнцезащитные компоненты"],
+  price: 1650
 },
 {
-  img: '/assets/gift-set-2.png',
+  img: 'gift-set-2.png',
   category: 'для нормальной кожи',
   name: 'Подарочный набор №1',
   description: 'Набор, состоящий из увлажняющего крема и маски.',
-  compound: 'Состав:',
-  compoundItem1: "воски",
-  compoundItem2: "минералы",
-  compoundItem3: "масла",
-  priceLabel: "Цена",
-  price: '4750 &#8381'
+  compound: ["воски", "минералы", "масла"],
+  price: 4750
 },
 {
-  img: '/assets/gift-set-5.png',
+  img: 'gift-set-5.png',
   category: 'для нормальной кожи',
   name: 'Подарочный набор №5',
   description: 'Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.',
-  compound: 'Состав:',
-  compoundItem1: "воски",
-  compoundItem2: "минералы",
-  compoundItem3: "масла",
-  priceLabel: "Цена",
-  price: '7520 &#8381'
+  compound: ["воски", "минералы", "масла"],
+  price: 7520
 }
 ]
